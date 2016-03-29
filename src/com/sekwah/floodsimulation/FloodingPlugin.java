309e0752b22e5bd9679b7e411516b2b8231acd6d
Alastair <alastair.hawkes@gmail.com>
2016-03-29 17:30:28 +0100

    Initial commit

---
+++ src/com/sekwah/floodsimulation/FloodingPlugin.java
@@ -0,0 +1,20 @@
+package com.sekwah.floodsimulation;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by on 29/03/2016.
+ *
+ * @author sekwah41
+ */
+public class FloodingPlugin extends JavaPlugin {
+
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+
+    }
+
+}
